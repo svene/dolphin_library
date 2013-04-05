a401a358e0e7b67156e62a4712c478d84346b2e6
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-05 17:18:30 +0200

    added table for book selection

--- client/src/main/groovy/org/svenehrke/library/LibraryView.groovy
+++ client/src/main/groovy/org/svenehrke/library/LibraryView.groovy
@@ -1,20 +1,41 @@
 package org.svenehrke.library
 
+import javafx.collections.ObservableList
+import javafx.collections.FXCollections
 import org.opendolphin.core.client.ClientDolphin
-import static groovyx.javafx.GroovyFX.start
+import org.opendolphin.core.client.ClientPresentationModel
 
+import static groovyx.javafx.GroovyFX.start
+import static org.opendolphin.binding.JavaFxUtil.value
+
+@SuppressWarnings("GroovyAssignabilityCheck")
 class LibraryView {
 	static show(ClientDolphin clientDolphin) {
+
+		ObservableList<ClientPresentationModel> observableBooks = FXCollections.observableArrayList()
 
 		start { app ->
 			def sgb = delegate
 			stage title:'Dolphin Library', {
 				scene width: 1000, height: 600, stylesheets: 'main.css', {
 					splitPane  {
-						dividerPosition(index: 0, position: 0.2)
+						dividerPosition(index: 0, position: 0.4)
+						vbox alignment: 'top_center', {
+							label "Books", id: 'bookListTitle'
+							tableView id: 'books', selectionMode: 'single', vgrow: 'always', {
+								value 'name',  tableColumn('Title', prefWidth: sgb.bind(books.width() / 2) )
+								value 'total', tableColumn('Author',     prefWidth: sgb.bind(books.width() / 2) )
+							}
+						}
+						stackPane {
+							text "Please select a Book", id: 'welcome'
+							tabPane id:'bookTabs'
+						}
 					}
 				}
 			}
+
+			books.items = observableBooks
 
 			primaryStage.show()
 		}
