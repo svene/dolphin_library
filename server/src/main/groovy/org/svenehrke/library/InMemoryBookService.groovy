b96ef46659165cea6ded2ccbffb8d7871b72cbd9
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-05 21:24:18 +0200

    using groovy style to make DTO creation much more readable

--- server/src/main/groovy/org/svenehrke/library/InMemoryBookService.groovy
+++ server/src/main/groovy/org/svenehrke/library/InMemoryBookService.groovy
@@ -1,28 +1,15 @@
 package org.svenehrke.library
-
 import org.opendolphin.core.server.DTO
 import org.opendolphin.core.server.Slot
-import static org.svenehrke.library.Constants.ATT.AUTHOR
-import static org.svenehrke.library.Constants.ATT.DOMAIN_ID
-import static org.svenehrke.library.Constants.ATT.TITLE
+
+import static org.svenehrke.library.Constants.ATT.*
 
 class InMemoryBookService implements IBookService {
 	@Override
 	List<DTO> list() {
-		List<DTO> books = new LinkedList<DTO>();
-		books.addAll(
-			new DTO(
-				new Slot(DOMAIN_ID, 1),
-				new Slot(TITLE, 'Refactoring'),
-				new Slot(AUTHOR, 'Martin Fowler'),
-			),
-			new DTO(
-				new Slot(DOMAIN_ID, 2),
-				new Slot(TITLE, 'Design Patterns'),
-				new Slot(AUTHOR, 'Gang of Four'),
-			)
-		);
-
-		books
+		[
+			[(DOMAIN_ID): 1, (TITLE): 'Refactoring', (AUTHOR): 'Martin Fowler'],
+			[(DOMAIN_ID): 1, (TITLE): 'Design Patterns', (AUTHOR): 'Gang of Four'],
+		].collect { new DTO(Slot.slots(it))  }
 	}
 }
