1be046720ea3f86d31d5252c5e32547992f25587
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-05 16:31:58 +0200

    started with css for background

--- client/src/main/groovy/org/svenehrke/library/LibraryView.groovy
+++ client/src/main/groovy/org/svenehrke/library/LibraryView.groovy
@@ -9,7 +9,10 @@ class LibraryView {
 		start { app ->
 			def sgb = delegate
 			stage title:'Dolphin Library', {
-				scene width: 1000, height: 600/*, stylesheets:"CrudDemo.css"*/, {
+				scene width: 1000, height: 600, stylesheets: 'main.css', {
+					splitPane  {
+						dividerPosition(index: 0, position: 0.2)
+					}
 				}
 			}
 
