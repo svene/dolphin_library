dce9af71213fcd0485c5c869a4f15ef34f38aace
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-05 22:31:24 +0200

    using DOMAIN_ID as id for PM and not index of entry in list

--- server/src/main/groovy/org/svenehrke/library/PullRegistrarAction.groovy
+++ server/src/main/groovy/org/svenehrke/library/PullRegistrarAction.groovy
@@ -1,8 +1,11 @@
 package org.svenehrke.library
+
 import org.opendolphin.core.server.DTO
+import org.opendolphin.core.server.Slot
 import org.opendolphin.core.server.action.DolphinServerAction
 import org.opendolphin.core.server.comm.ActionRegistry
 
+import static org.svenehrke.library.Constants.ATT.DOMAIN_ID
 import static org.svenehrke.library.Constants.CMD.PULL
 import static org.svenehrke.library.Constants.TYPE.BOOK
 import static org.svenehrke.library.Constants.pmId
@@ -17,7 +20,8 @@ class PullRegistrarAction extends DolphinServerAction {
 			List<DTO> books = bookService.list()
 
 			books.eachWithIndex { dto, index ->
-				presentationModel pmId(BOOK, index), BOOK, dto
+				Slot slot = dto.slots.find {it.propertyName == DOMAIN_ID}
+				presentationModel pmId(BOOK, slot.value), BOOK, dto
 			}
 		}
 
